d31529d56de4238e34bee037d3b6665a9de31d3b
shivam6797 <shivampratap6797@gmail.com>
2025-02-06 21:52:05 +0530

    update app title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Notes App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
